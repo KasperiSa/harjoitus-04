934cad52248c29b22a3d93393c9af0842647e05e
KasperiSa <crakizor@luukku.com>
2014-09-29 23:53:51 +0300

    harj4 kommenttien lisäys
    selitykset on lisätty kopsattuun esimerkkiin

---
+++ harj4kommentit.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>		/* Mahdollistetaan input ja output aliohjelmakirjasto*/
+//  using namespace std;		/* k‰ytet‰‰n standard template librarya */
+// #define MAX 10					/*"Symbolinen vakio" MAX on arvoltaan 10*/
+// const int max = 20;					/*"Nimetty vakio" max on arvoltaan 20*/
+// int summa;								/*On olemassa kokonaisluku nimelt‰‰n "summa"*/
+// void Laske(int, int);	/*on olemassa ohjelma nimelt‰‰n "Laske", jossa on kaksi kokonaislukua*/
+
+// void main() /*ensimm‰innen ohjelma alkaa*/
+// {
+// 	int luku1 = 2; /*on olemassa kokonaisluku nimelt‰‰n luku1 jonka arvo on 2*/
+//	int luku2;  /*on olemassa kokonaisluku nimelt‰‰n luku2*/
+//	cout << "Syˆt‰ luku: "; /*Tulostetaan teksti "Syˆt‰ luku "*/
+//	cin >> luku2; /*K‰ytt‰j‰ syˆtt‰‰ arvon josta tulee luku2:n arvo*/
+//	Laske(luku1, luku2); /*Ajaa "Laske" nimisen ohjelman joka laskee kaksi m‰‰ritetty‰ 
+//						 kokonaislukua yhteen ne ovat luku1(sen arvo on m‰‰ritelty olevan 2) ja 
+//						 luku2(Ohjelman ajaja syˆtt‰‰ luvun)*/
+//	if (summa < MAX)
+//		cout<<"Summa on pienempi kuin "<< MAX;
+//	if (summa < max)
+//		cout<<"Summa on pienempi kuin "<< max;
+
+
+// }
+// void Laske(int eka, int toka) /*Laske ohjelmassa on kaksi kokonaislukua joiden nimet ovat eka ja toka*\
+								
+// {
+// 	summa = eka + toka; /*Laske ohjelmassa kerrotaan ett‰ summa tarkoittaa ekan ja tokan laskemista yhteen*\
+// }
